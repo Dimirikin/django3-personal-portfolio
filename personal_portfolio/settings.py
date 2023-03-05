e10011fd58ae17b1010cfa9695b95ace111ee624
Volodymyr Rubeikin <vladimir.rubeykin@gmail.com>
2023-03-05 15:59:13 +0100

    change local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -129,6 +129,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 try:
-    from .local_setitngs import *
+    from .local_settings import *
 except ImportError:
     print("Looks like no local file. You must be on production")
